e81d65d15054b7f8c003de2ed9b2e7eb94fe70da
Grism Patel <grismpatel@hotmail.com>
2019-12-20 15:34:49 -0500

    seperated them in class based on there functionality

--- test.py
+++ test.py
@@ -4,109 +4,122 @@ import glob
 import os
 import datetime
 
-csv_files = glob.glob(os.path.join(os.getcwd() + '/input-directory', '*.csv'))
-
-spark = SparkSession \
-        .builder \
-        .appName("Grism Scoir") \
-        .getOrCreate()
-spark.sparkContext.setLogLevel('WARN')
-
-col_info = [
-        {
-            "col_name": "INTERNAL_ID",
-            "datatype": "int",
-            "nullable": False,
-            "condition": {
-                "len": 8
+class Service():
+    def __init__(self):
+        self.csv_files = glob.glob(os.path.join(os.getcwd() + '/input-directory', '*.csv'))
+        self.col_info = [
+            {
+                "col_name": "INTERNAL_ID",
+                "datatype": "int",
+                "nullable": False,
+                "condition": {
+                    "len": 8
+                }
+            },
+            {
+                "col_name": "FIRST_NAME",
+                "datatype": "string",
+                "nullable": False,
+                "condition": {
+                    "max_len": 15
+                }
+            },
+            {
+                "col_name": "MIDDLE_NAME",
+                "datatype": "string",
+                "nullable": True,
+                "condition": {
+                    "max_len": 15
+                }
+            },
+            {
+                "col_name": "LAST_NAME",
+                "datatype": "string",
+                "nullable": False,
+                "condition": {
+                    "max_len": 15
+                }
+            },
+            {
+                "col_name": "PHONE_NUM",
+                "datatype": "string",
+                "nullable": False,
+                "condition": {
+                    "max_len": 15
+                }
             }
-        },
-        {
-            "col_name": "FIRST_NAME",
-            "datatype": "string",
-            "nullable": False,
-            "condition": {
-                "max_len": 15
-            }
-        },
-        {
-            "col_name": "MIDDLE_NAME",
-            "datatype": "string",
-            "nullable": True,
-            "condition": {
-                "max_len": 15
-            }
-        },
-        {
-            "col_name": "LAST_NAME",
-            "datatype": "string",
-            "nullable": False,
-            "condition": {
-                "max_len": 15
-            }
-        },
-        {
-            "col_name": "PHONE_NUM",
-            "datatype": "string",
-            "nullable": False,
-            "condition": {
-                "max_len": 15
-            }
-        }
-    ]
+        ]
 
-def condition(dfRaw, col_condition, col_name):
+class Spark_Service():
+    def __init__(self):
+        self.spark = SparkSession \
+                    .builder \
+                    .appName("Grism Scoir") \
+                    .getOrCreate()
+        self.spark.sparkContext.setLogLevel('WARN')
 
-    if "len" in col_condition:
-        dfTemp = dfRaw.filter(length(col(col_name)) != col_condition['len'])
-        dfTemp = dfTemp.withColumn("Error Details", lit("The len of the coloumn does not match the given condition"))
-        print ("FROM LEN:", dfTemp.show(10))
+    def file_to_df(self, file, format_of_file, delimeter):
+        dfRaw = self.spark.read.load(file, format=format_of_file, sep=delimeter, header= True)
+        return dfRaw
 
-    elif "max_len" in col_condition and "min_len" in col_condition:
-        dfTemp = dfRaw.filter(length(col(col_name)) > col_condition['max_len'] & 
-                              length(col(col_name)) < col_condition['min_len'])
-        dfTemp = dfTemp.withColumn("Error Details", lit("The len of the coloum does not match the given constaints"))
-        print ("Should not be printed:", dfTemp.show(10))
+    def validate(self, dfRaw, col_info, dfError):
+        if not col_info['nullable']:
+            dfCorrectData = dfRaw.withColumn(col_info['col_name'], dfRaw[col_info['col_name']].cast(col_info['datatype'])).na.drop(subset=[col_info['col_name']])
+            dftemp = dfRaw.exceptAll(dfCorrectData)
+            dftemp = dftemp.withColumn("Error Details", lit("{col_name} is null or the data type provided is wrong".format(col_name = col_info['col_name'])))
+            dfError = dfError.union(dftemp)
 
-    elif "max_len" in col_condition or "min_len" in col_condition:
-        if "max_len" in col_condition:
-            dfTemp = dfRaw.filter(length(col(col_name)) > col_condition['max_len'])
+            dfCondition = self.condition(dfRaw, col_info['condition'], col_info['col_name'])
+            dfError = dfError.union(dfCondition)
+        return dfError
+
+    def condition(self, dfRaw, col_condition, col_name):
+
+        if "len" in col_condition:
+            dfTemp = dfRaw.filter(length(col(col_name)) != col_condition['len'])
+            dfTemp = dfTemp.withColumn("Error Details", lit("The len of the coloumn does not match the given condition"))
+
+        elif "max_len" in col_condition and "min_len" in col_condition:
+            dfTemp = dfRaw.filter(length(col(col_name)) > col_condition['max_len'] & 
+                                length(col(col_name)) < col_condition['min_len'])
             dfTemp = dfTemp.withColumn("Error Details", lit("The len of the coloum does not match the given constaints"))
-            print ("with max length: ", dfTemp.show(10))
 
-        elif "min_len" in col_condition:
-            dfTemp = dfRaw.filter(length(col(col_name)) < col_condition['min_len'])
-            dfTemp = dfTemp.withColumn("Error Details", lit("The len of the coloum does not match the given constaints"))
-            print ("this also should not printL ", dfTemp.show(10))
-    
-    if "format" in col_condition:
-        pass
-    
-    print ("From condition: ", dfTemp.count())
-    return dfTemp
+        elif "max_len" in col_condition or "min_len" in col_condition:
+            if "max_len" in col_condition:
+                dfTemp = dfRaw.filter(length(col(col_name)) > col_condition['max_len'])
+                dfTemp = dfTemp.withColumn("Error Details", lit("The len of the coloum does not match the given constaints"))
 
-def validate(dfRaw, col_info, dfError):
-    if not col_info['nullable']:
-        dfCorrectData = dfRaw.withColumn(col_info['col_name'], dfRaw[col_info['col_name']].cast(col_info['datatype'])).na.drop(subset=[col_info['col_name']])
-        dftemp = dfRaw.exceptAll(dfCorrectData)
-        dftemp = dftemp.withColumn("Error Details", lit("{col_name} is null or the data type provided is wrong".format(col_name = col_info['col_name'])))
-        dfError = dfError.union(dftemp)
+            elif "min_len" in col_condition:
+                dfTemp = dfRaw.filter(length(col(col_name)) < col_condition['min_len'])
+                dfTemp = dfTemp.withColumn("Error Details", lit("The len of the coloum does not match the given constaints"))
+        
+        if "format" in col_condition:
+            pass
+        
+        return dfTemp
 
-        dfCondition = condition(dfRaw, col_info['condition'], col_info['col_name'])
-        dfError = dfError.union(dfCondition)
-    return dfError
+if __name__ == "__main__":
+    s = Service()
 
-for csv in csv_files:
-    print ("###################################### NEW FILE #########################################")
-    print ("csv file:", csv)
+    spark_service = Spark_Service()
+    spark = SparkSession \
+            .builder \
+            .appName("Grism Scoir") \
+            .getOrCreate()
+    spark.sparkContext.setLogLevel('WARN')
 
-    dfRaw = spark.read.load(csv, format='csv', sep=',', header= True)
-    dfRaw = dfRaw.withColumn("Error Details", lit("Everythign is Awesome"))
+    for csv in s.csv_files:
+        print ("###################################### NEW FILE #########################################")
+        print ("csv file:", csv)
 
-    dfError = (reduce(lambda dfError, col_info: validate(dfRaw, col_info, dfError), col_info, dfRaw))
-    dfError = dfError.exceptAll(dfRaw)
+        dfRaw = spark_service.file_to_df(csv, 'csv', ',')
+        dfRaw = dfRaw.withColumn("Error Details", lit("Everythign is Awesome"))
 
-    if dfError.count() != 0:
-        print ("Error File: ", csv)
-        dfError.show()
-        # put it into error-directory folder
+        dfError = (reduce(lambda dfError, col_info: spark_service.validate(dfRaw, col_info, dfError), s.col_info, dfRaw))
+        dfError = dfError.exceptAll(dfRaw)
+
+        if dfError.count() != 0:
+            print ("Error File: ", csv)
+            dfError.show()
+            # put it into error-directory folder
+
