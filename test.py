a2c3fe3579f444e5faa49e23a728a9a838bd4c52
Grism Patel <grismpatel@hotmail.com>
2019-12-20 12:30:54 -0500

    added a test col to check for nulls and wrong datatype

--- test.py
+++ test.py
@@ -13,3 +13,15 @@ spark.sparkContext.setLogLevel('WARN')
 
 for csv in csv_files:
     print ("csv file:", csv)
+
+    dfRaw = spark.read.load(csv, format='csv', sep=',', header= True)
+
+    print ("File Count: ", dfRaw.count())
+
+    # checking for nulls and datatype
+    dfCorrectData = dfRaw.withColumn("INTERNAL_ID", dfRaw["INTERNAL_ID"].cast("int")).na.drop(subset=["INTERNAL_ID"])
+    dfError = dfRaw.exceptAll(dfCorrectData)
+
+    if dfError.count() > 1:
+        print ("Error File: ", csv)
+        # put it into error-directory folder
